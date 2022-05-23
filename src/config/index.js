9ddd941dc99592e8699f29276cc25e33c7dddaab
yang <807037603@qq.com>
2022-05-23 19:45:43 +0800

    生产环境基本url修改

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ export default {
         //本地开发环境url,本地用的是服务器的代理,来访问api
         dev: 'http://localhost:8080',
         //生产环境url
-        pro: '',
+        pro: 'http://rcc.thisyang.top',
     }
 }
