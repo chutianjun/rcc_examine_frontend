812e534b72a6ca2fb5e77b9ef037bf8cdf342d69
yang <807037603@qq.com>
2022-05-23 22:11:27 +0800

    修改为本地的ip 127.0.0.1

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
     baseURL: {
         //本地开发环境url,本地用的是服务器的代理,来访问api
-        dev: 'http://localhost:8080',
+        dev: 'http://127.0.0.1:8080',
         //生产环境url
         pro: 'http://rcc.thisyang.top',
     }
--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,32 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
+
     transpileDependencies: true,
-    devServer:{
+    devServer: {
+        //ip地址
+        host: '127.0.0.1',
+        //端口号
+        port: 8080,
         //代理设置
-        proxy:{
+        proxy: {
             //首页
-            '/apihome':{
-                target:'http://localhost:3000',
-                secure:false,
-                changeOrigin:true,
-                pathRewrite:{
+            '/apihome': {
+                target: 'http://127.0.0.1:3000',
+                secure: false,
+                changeOrigin: true,
+                pathRewrite: {
                     //把apihome替换为 空
-                    '^/apihome':''
+                    '^/apihome': ''
                 }
             },
             //api访问
-            '/api':{
-                target:'http://localhost:3000',
-                secure:false,
-                changeOrigin:true,
-                pathRewrite:{
+            '/api': {
+                target: 'http://127.0.0.1:3000',
+                secure: false,
+                changeOrigin: true,
+                pathRewrite: {
                     //把 api 开头 替换为 /web/v1
-                    '^/api':'/web/v1'
+                    '^/api': '/web/v1'
                 }
             }
         }
